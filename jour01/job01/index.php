c5f85b6baf27972ab6998391eb11425656cc859b
nicolas-panis <nicolas.panis@laplateforme.io>
2023-01-24 11:45:28 +0100

    jour01 - job01

--- jour01/job01/index.php
+++ jour01/job01/index.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Index PHP</title>
-        <script src= "script.js"></script>
+        <script> alert("Hello Javascript !");</script>
     </head>
 
     <body>
